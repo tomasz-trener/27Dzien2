573fb97dff38a9648564a505c63bbe17777185fd
kurs <kurs@DESKTOP-V3HDKPQ>
2023-12-19 10:59:39 +0100

    q

---
+++ P06KonwersjaTablicOrazNapisow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06KonwersjaTablicOrazNapisow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string zdanie = "ala ma kota";
+
+            // SPLIT - rozdzielenie 
+
+            string[] wyrazy = zdanie.Split(' ');
+
+            string zdanie2 = "ala!$ma**kota";
+
+            string[] separatory = { "!$", "**" };
+
+            string[] wyrazy2 = zdanie2.Split(separatory,StringSplitOptions.RemoveEmptyEntries);
+
+            // JOIN - łaczenie 
+
+            string[] tablica = { "jan", "ola", "adam" };
+
+            string wynik = string.Join(" - ", tablica);
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+        }
+    }
+}
